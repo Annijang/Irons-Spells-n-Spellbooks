f3ac1dd679b1e40101b468db0bb944ad6cdf63d9
Christian <cmb@bitfly.org>
2023-07-06 16:37:43 -0500

    test legs

--- src/main/java/io/redspace/ironsspellbooks/entity/mobs/keeper/KeeperModel.java
+++ src/main/java/io/redspace/ironsspellbooks/entity/mobs/keeper/KeeperModel.java
@@ -3,7 +3,13 @@ package io.redspace.ironsspellbooks.entity.mobs.keeper;
 import io.redspace.ironsspellbooks.IronsSpellbooks;
 import io.redspace.ironsspellbooks.entity.mobs.abstract_spell_casting_mob.AbstractSpellCastingMob;
 import io.redspace.ironsspellbooks.entity.mobs.abstract_spell_casting_mob.AbstractSpellCastingMobModel;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.geom.PartNames;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.WalkAnimationState;
+import software.bernie.geckolib.core.animatable.model.CoreGeoBone;
+import software.bernie.geckolib.core.animation.AnimationState;
 
 public class KeeperModel extends AbstractSpellCastingMobModel {
     public static final ResourceLocation TEXTURE = new ResourceLocation(IronsSpellbooks.MODID, "textures/entity/keeper/keeper.png");
@@ -17,5 +23,37 @@ public class KeeperModel extends AbstractSpellCastingMobModel {
     @Override
     public ResourceLocation getModelResource(AbstractSpellCastingMob object) {
         return modelResource;
+    }
+
+    @Override
+    public void setCustomAnimations(AbstractSpellCastingMob entity, long instanceId, AnimationState<AbstractSpellCastingMob> animationState) {
+        super.setCustomAnimations(entity, instanceId, animationState);
+
+        CoreGeoBone rightLeg = this.getAnimationProcessor().getBone(PartNames.RIGHT_LEG);
+        CoreGeoBone leftLeg = this.getAnimationProcessor().getBone(PartNames.LEFT_LEG);
+
+
+        if (Minecraft.getInstance().isPaused())
+            return;
+
+        float partialTick = animationState.getPartialTick();
+        WalkAnimationState walkAnimationState = entity.walkAnimation;
+        float pLimbSwingAmount = 0.0F;
+        float pLimbSwing = 0.0F;
+        if (entity.isAlive()) {
+            pLimbSwingAmount = walkAnimationState.speed(partialTick);
+            pLimbSwing = walkAnimationState.position(partialTick);
+            if (entity.isBaby()) {
+                pLimbSwing *= 3.0F;
+            }
+
+            if (pLimbSwingAmount > 1.0F) {
+                pLimbSwingAmount = 1.0F;
+            }
+        }
+//        rightLeg.setRotX(Mth.cos(pLimbSwing * 0.6662F) * 1.4F * pLimbSwingAmount / f);
+        rightLeg.updatePosition(0, Mth.cos(pLimbSwing * 0.6662F) * 4F * pLimbSwingAmount, -Mth.sin(pLimbSwing * 0.6662F) * 4F * pLimbSwingAmount);
+        leftLeg.updatePosition(0, Mth.cos(pLimbSwing * 0.6662F - Mth.PI) * 4F * pLimbSwingAmount, -Mth.sin(pLimbSwing * 0.6662F - Mth.PI) * 4F * pLimbSwingAmount);
+
     }
 }
